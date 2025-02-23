595fc2631a7a663371622d5b2fc4859963b1d626
zz <13818448+qqggdzh@user.noreply.gitee.com>
2025-02-23 21:07:29 +0800

    项目初始化构建

---
+++ src/main/java/org/example/nutri_guide_background/NutriGuideBackgroundApplication.java
@@ -0,0 +1,13 @@
+package org.example.nutri_guide_background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriGuideBackgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriGuideBackgroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nutri_guide_background/NutriGuideBackgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nutri_guide_background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriGuideBackgroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
